7401c765c71fd27db694f1940ae2a99610adec46
Aldiyar <abaibogurov@gmail.com>
2025-02-04 19:30:07 -0500

    Add project files.

---
+++ GymManagement/Utilities/EmailService.cs
@@ -0,0 +1,80 @@
+﻿using GymManagement.ViewModels;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit.Text;
+using MimeKit;
+
+namespace GymManagement.Utilities
+{
+    public class EmailService
+    {
+        /// <summary>
+        /// This implements the IEmailService from
+        /// Microsoft.AspNetCore.Identity.UI.Services for the Identity System
+        /// </summary>
+        public class EmailSender : IEmailSender
+        {
+            private readonly IEmailConfiguration _emailConfiguration;
+            private readonly ILogger<EmailSender> _logger;
+
+            public EmailSender(IEmailConfiguration emailConfiguration, ILogger<EmailSender> logger)
+            {
+                _emailConfiguration = emailConfiguration;
+                _logger = logger;
+            }
+
+            /// <summary>
+            /// Asynchronously builds and sends a message to a single recipient.
+            /// </summary>
+            /// <param name="email"></param>
+            /// <param name="subject"></param>
+            /// <param name="htmlMessage"></param>
+            /// <returns></returns>
+            public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+            {
+                var message = new MimeMessage();
+                message.To.Add(new MailboxAddress(email, email));
+                message.From.Add(new MailboxAddress(_emailConfiguration.SmtpFromName, _emailConfiguration.SmtpUsername));
+
+                message.Subject = subject;
+                message.Body = new TextPart(TextFormat.Html)
+                {
+                    Text = htmlMessage
+                };
+
+                await SendEmailMessageAsync(message);
+
+            }
+
+            /// <summary>
+            /// Sends the MimeMessage
+            /// </summary>
+            /// <param name="theMessage">The MimeMessage</param>
+            /// <returns></returns>
+            public async Task SendEmailMessageAsync(MimeMessage theMessage)
+            {
+                try
+                {
+                    using var emailClient = new SmtpClient();
+
+                    emailClient.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.SmtpPort, false);
+
+
+                    emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                    emailClient.Authenticate(_emailConfiguration.SmtpUsername, _emailConfiguration.SmtpPassword);
+
+                    await emailClient.SendAsync(theMessage);
+
+                    emailClient.Disconnect(true);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.GetBaseException().Message);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ GymManagement/Utilities/MaintainURL.cs
@@ -0,0 +1,35 @@
+﻿namespace GymManagement.Utilities
+{
+    public static class MaintainURL
+    {
+        /// <summary>
+        /// Maintain the URL for an Index View including filter, sort and page information.
+        /// Depends on the CookieHelper Utility.
+        /// </summary>
+        /// <param name="httpContext">the HttpContext from the Controller</param>
+        /// <param name="ControllerName">The Name of the Controller</param>
+        /// <returns>The Index URL with paramerters if required</returns>
+        public static string ReturnURL(HttpContext httpContext, string ControllerName)
+        {
+            string cookieName = ControllerName + "URL";
+            string SearchText = "/" + ControllerName + "?";
+            //Get the URL of the page that sent us here
+            string returnURL = httpContext.Request.Headers["Referer"].ToString();
+            if (returnURL.Contains(SearchText))
+            {
+                //Came here from the Index with some parameters
+                //Save the Parameters in a Cookie
+                returnURL = returnURL[returnURL.LastIndexOf(SearchText)..];
+                CookieHelper.CookieSet(httpContext, cookieName, returnURL, 30);
+                return returnURL;
+            }
+            else
+            {
+                //Get it from the Cookie
+                returnURL = httpContext.Request.Cookies[cookieName] ?? "/" + ControllerName;
+                return returnURL;
+            }
+        }
+    }
+
+}
